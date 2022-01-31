0a3d473e8cb8c4a3a3c546f91d0b99667db82e73
swaroop18122002 <swaroop.rajwal18@gmail.com>
2022-01-31 16:00:46 +0530

    🙂 I hope this works fine now

--- components/Head.tsx
+++ components/Head.tsx
@@ -25,20 +25,20 @@ const Head = (props: Props) => {
       </title>
       {/* <!-- Primary Meta Tags --> */}
       <meta name="title" content={props.userName || defaultTitle}/>
-      <meta name="description" content={`${defaultDescription}`}/>
+      <meta name="description" content="Have a nicer look at your github profile, this is an app made on top of github users api, biult with nextjs, typescript and chartjs."/>
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website"/>
       <meta property="og:url" content={buildUrl(props.userName) || defaultUrl}/>
       <meta property="og:title" content={props.userName || defaultTitle}/>
-      <meta property="og:description" name="description" content={`${defaultDescription}`}/>
+      <meta property="og:description" content="Have a nicer look at your github profile, this is an app made on top of github users api, biult with nextjs, typescript and chartjs."/>
       {props.showImage && <meta property="og:image" content="/not_search_page.png"/>}
 
       {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content={buildUrl(props.userName) || defaultUrl}/>
       <meta property="twitter:title" content={props.userName || defaultTitle}/>
-      <meta property="twitter:description" name="description" content={`${defaultDescription}`}/>
+      <meta property="twitter:description" content="Have a nicer look at your github profile, this is an app made on top of github users api, biult with nextjs, typescript and chartjs."/>
       {props.showImage && <meta property="twitter:image" content="/not_search_page.png"/>}
     </NHead>
   )
